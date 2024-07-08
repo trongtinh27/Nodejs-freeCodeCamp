14a8f37345d5f3a69da47c0516b4e3aea3c8df7c
ALVIN NTSAKO MALULEKE <105872985+Maluleke062@users.noreply.github.com>
2024-07-08 21:14:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+const cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+// Root endpoint. Display index file
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+
+// Handle returning a timestamp
+app.get('/api/timestamp/:date?', (req, res) => {
+    // Store our date response. This will default to the current datetime
+    let date = new Date();
+
+    // Check if the optional date parameter was provided
+    if (req.params.date) {
+        // Convert the date parameter to a string
+        let unixDate = +req.params.date;
+
+        // Check if the date passed is unix time. If it's not, use the date string provided
+        date = isNaN(unixDate) ? new Date(req.params.date) : new Date(unixDate);
+
+        // Check if the date created is valid. Throw an error if it's an invalid date
+        if (!(date instanceof Date) || isNaN(date.getTime())) return res.json({ error: "Invalid Date" });
+    }
+
+    // Return the unix and UTC time
+    return res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// Create a listener to handle requests
+const listener = app.listen(process.env.PORT, () => console.log('Your app is listening on port ' + listener.address().port));
