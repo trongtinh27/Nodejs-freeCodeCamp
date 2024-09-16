7fc183d822891a91dc40546373602fc2c77038b6
Kanha Dewangan <166921193+kanhadewangan@users.noreply.github.com>
2024-09-16 19:14:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({new Date()})
+  
 });
 
 
