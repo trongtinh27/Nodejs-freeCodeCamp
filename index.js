adc6ffe28a47fe9ffe85901b68f2b778a7bef404
siphonhata <siphonhata@gmail.com>
2024-11-12 12:00:33 +0200

    added api component

--- index.js
+++ index.js
@@ -18,12 +18,18 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/", function (req, res) {
+  const now = new Date()
+  res.json({
+    "unix": now.getTime(),
+      "utc": now.toUTCString()
+  })
+})
 
 app.get("/api/:date_string", function (req, res)
 {
