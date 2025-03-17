98e8823e3c8d14d8884fe94a72a0a93a18035c93
Hudson512 <hmateque@gmail.com>
2025-03-17 15:33:18 +0100

    terminado

--- index.js
+++ index.js
@@ -24,6 +24,47 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+function identifyFormat(value) {
+  if (!isNaN(Number(value)) && value.length >= 12) {
+    return "timestamp";
+  } else if (/^\d{4}-\d{2}-\d{2}$/.test(value)) {
+    return "date";
+  } else {
+    return "invalid";
+  }
+}
+
+// Timestamp Microservice
+app.get("/api/:date_string?", function (req, res) {
+  let date_string = req.params.date_string;
+
+  let date;
+  let result = identifyFormat(date_string);
+
+  if (date_string === undefined) {
+    date = new Date();
+  } else if (result === "timestamp") {
+    date = new Date(parseInt(date_string));
+  } else if (result === "date") {
+    date = new Date(date_string);
+  }
+  else {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+
+  if (result === "timestamp")
+  {
+    res.json({ unix: date_string, utc: new Date(date).toUTCString() });
+    return;
+  }
+  else if (result === "date") 
+  {
+    res.json({ unix: parseInt(date.getTime()), utc: date.toUTCString() });
+  }
+}
+);
+
 
 
 // Listen on port set in environment variable or default to 3000
